dcf8877b89bdbcaef8a039bffc3fe2556e0a773c
super-valter <valter@valtinho.com.br>
2023-12-05 22:24:00 -0300

    subindo pasta go/kafka

---
+++ go/cmd/trade/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/super-valter/imersaofullcycle/go/internal/infra/kafka"
+	"github.com/super-valter/imersaofullcycle/go/internal/market/dto"
+	"github.com/super-valter/imersaofullcycle/go/internal/market/entity"
+	"github.com/super-valter/imersaofullcycle/go/internal/market/transformer"
+)
+
+func main() {
+	ordersIn := make(chan *entity.Order)
+	ordersOut := make(chan *entity.Order)
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+
+	kafkaMsgChan := make(chan *ckafka.Message)
+	configMap := &ckafka.ConfigMap{
+		"boostrap.servers":  "host.docker.internal:9094",
+		"group.id":          "mygroup",
+		"auto.offset.reset": "latest",
+	}
+	producer := kafka.NewKafkaProducer(configMap)
+	kafka := kafka.NewConsumer(configMap, []string{"input"})
+
+	go kafka.Consumer(kafkaMsgChan) //T2
+
+	// recebe do canal do kafka, joga no input, processa joga no output e depois publica no kafka
+	book := entity.NewBook(ordersIn, ordersOut, wg)
+	go book.Trade() //T3
+
+	go func() {
+		for msg := range kafkaMsgChan {
+			wg.Add(1)
+			fmt.Println(string(msg.Value))
+			tradeInput := dto.TradeInput{}
+			err := json.Unmarshal(msg.Value, &tradeInput)
+			if err != nil {
+				panic(err)
+			}
+			order := transformer.TransformInput(tradeInput)
+			ordersIn <- order
+		}
+	}()
+
+	for res := range ordersOut {
+		output := transformer.TransformerOutput(res)
+		outputJson, err := json.MarshalIndent(output, "", "  ")
+		fmt.Println(string(outputJson))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		producer.Publish(outputJson, []byte("orders"), "output")
+	}
+}
